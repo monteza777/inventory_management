2bde2aeb02cc56ab7c2c9c615bfd3e0f43e13424
Mitchieboy <montezamitch07@gmail.com>
2017-12-16 01:50:23 +0800

    save head & details transaction

--- app/Http/Controllers/TransactionHeadController.php
+++ app/Http/Controllers/TransactionHeadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction_head;
 use App\Transaction_detail;
 use Illuminate\Http\Request;
 class TransactionHeadController extends Controller
@@ -34,7 +35,33 @@ class TransactionHeadController extends Controller
      */
     public function store(Request $request)
     {
-        for ($i=0; $i < count($request['qty']); ++$i) 
+        $customer = new Transaction_head;
+        $customer->discount= $request['vat'];
+        $customer->discount= $request['discount'];
+        $customer->mode_of_payment= $request['mode_of_payment'];
+        $customer->cumulative_amount= $request['cumulative_amount'];
+        $customer->amount_tendered= $request['amount_tendered'];
+        $customer->change_due= $request['change_due'];
+        $customer->customer_name= $request['customer_name'];
+        $customer->customer_phone= $request['customer_phone'];
+        $customer->customer_email= $request['customer_email'];
+        // $customer->save();
+        if($customer->save()){
+            $id= $customer->id;
+            foreach($request->item_code as $key => $val){
+                $data = array('transaction_head_id'=>$id,
+                            'item_code'=>$request['item_code'] [$key],
+                            'qty'=>$request['qty'] [$key],
+                            'unit_price'=>$request['unit_price'] [$key],
+                            'total_price'=>$request['total_price'] [$key],
+                            'item_name'=> 'name',
+                            'available_quantity'=> 1,);
+                        Transaction_detail::insert($data);
+            }
+        }
+        return redirect()->route('transaction-mgmt.index');
+
+        /*for ($i=0; $i < count($request['qty']); ++$i) 
         {
             $items= new Transaction_detail;        
             $items->item_code = $request['item_code'][$i];
@@ -46,7 +73,7 @@ class TransactionHeadController extends Controller
             $items->available_quantity= 1;
             $items->save();
             return redirect()->route('transaction-mgmt.index');
-        }
+        }*/
     }
 
     /**
--- resources/views/transaction-mgmt/transaction_index.blade.php
+++ resources/views/transaction-mgmt/transaction_index.blade.php
@@ -3,72 +3,89 @@
 @section('head_title','Transaction')
 
 @section('content')
-
-
-<div id="app" class="container form-group well" v-cloak>
-
-<button class="btn btn-primary" v-on:click="seen = !seen" v-cloak> <i class="glyphicon" v-bind:class="[seen ? plusIcon: minusIcon]"></i>@{{ seen ? ' Add Transaction' : ' Hide Transaction'}}</button>
-	<div v-if="!seen">
-	<div class="col-md-10 col-lg-10 col-md-offset-2 col-lg-offset-2">
-		<form>
-		<div class="col-md-3 col-lg-3 form-group">
-			<label>Vat(%):</label>
-			<input type="text" class="form-control" placeholder="Vat">
-			<label>Cumulative Amount:</label>
-			<input type="text" class="form-control" placeholder="Cumulative Amount">
-			<label>Customer Name:</label>
-			<input type="text" class="form-control" placeholder="Customer Name">
+<body>
+<div id="app" class="container col-lg-12 col-md-12" v-cloak>
+	<section class="panel panel-default">
+		<div class="panel-heading col-lg-12 col-md-12">
+			<button class="btn btn-primary" v-on:click="seen = !seen" v-cloak> <i class="glyphicon" v-bind:class="[seen ? plusIcon: minusIcon]"></i>@{{ seen ? ' Add Transaction' : ' Hide Transaction'}}</button>
 		</div>
-		<div class="col-md-3 col-lg-3">
-			<label>Discount</label>
-			<input type="text" class="form-control" placeholder="Discount">
-			<label>Amount Tendered:</label>
-			<input type="text" class="form-control" placeholder="Amount Tendered">
-			<label>Customer Phone:</label>
-			<input type="text" class="form-control" placeholder="Customer Phone">
+		<div v-if="!seen">
+		<div class="panel-footer">
+			<form class="form-group" method="post" action="{{route('transaction-mgmt.store')}}">
+				{{csrf_field()}}
+				<div class="row">
+				<div class="col-lg-4 col-md-4">
+				<div class="form-group">
+					<label>Vat(%):</label>
+					<input type="text" class="form-control" placeholder="Vat" name="vat">
+					<label>Cumulative Amount</label>
+					<input type="text" class="form-control" placeholder="Cumulative Amount" name="cumulative_amount">
+					<label>Customer Name</label>
+					<input type="text" class="form-control" placeholder="Customer Name" name="customer_name">
+				</div>	
+				</div>
+				<div class="col-lg-4 col-md-4">
+				<div class="form-group">
+					<label>Discount(%):</label>
+					<input type="text" class="form-control" placeholder="Discount" name="discount">
+					<label>Amount Tendered:</label>
+					<input type="text" class="form-control" placeholder="Amount Tendered" name="amount_tendered">
+					<label>Customer Phone:</label>
+					<input type="text" class="form-control" name="customer_phone" placeholder="Customer Phone" name="customer_phone">
+				</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+				<div class="form-group">
+					<label>Mode of Payment</label>
+					<input type="text" class="form-control" placeholder="Mode of Payment" name="mode_of_payment">
+					<label>Change Due:</label>
+					<input type="text" class="form-control" placeholder="Change Due" name="change_due">
+					<label>Customer Email:</label>
+					<input type="text" class="form-control" placeholder="Customer Email" name="customer_email">
+				</div>
+				</div>
+			<div class="savebutton">
+			<button type="submit" class="btn btn-primary">
+		        Save
+		    </button>
+			</div>
+			<div class="col-lg-12 col-sm-12" v-for="(employee, index) in employees"><hr>
+			<div class="form-group">
+			<table class="table table-bordered ">
+	      	<thead>
+	      		<th>Item</th>
+	      		<th>Quantity</th>
+	      		<th>Unit Price</th>
+	      		<th>Total Price</th>
+	      		<th>
+	      			<i class="btn btn-xs btn-success glyphicon glyphicon-plus" @click="addNewItem"></i>
+	      		</th>
+	      	</thead>
+	      	<tbody>
+	      	<tr class="rounded">
+	      		<td>
+			      	<input type="text" name="item_code[]" class="form-control" placeholder="About" v-model="employee.name">
+	      		</td>
+	      		<td>
+			      	<input type="text" name="qty[]" class="form-control" placeholder="About" v-model="employee.job">
+	      		</td>
+	      		<td>
+			      	<input type="text" name="unit_price[]" class="form-control" placeholder="About" v-model="employee.about">
+	      		</td>
+	      		<td>
+			      	<input type="text" name="total_price[]" class="form-control" placeholder="About" v-model="employee.about">
+	      		</td>
+	      		<td><i class="glyphicon glyphicon-remove btn btn-xs btn-danger" @click="deleteForm"></i></td>
+
+	      	</tr>
+	      	</tbody>
+	      	</table>
+			</div>
+			</div>
+			</form>
 		</div>
-		<div class="col-md-3 col-lg-3">
-			<label>Mode of Payment:</label>
-			<input type="text" class="form-control" placeholder="Mode of Payment">
-			<label>Change Due:</label>
-			<input type="text" class="form-control" placeholder="Change Due">
-			<label>Customer Email:</label>
-			<input type="text" class="form-control" placeholder="Customer Email	"><hr>
 		</div>
-		</form>
-	</div>
-
-<button class="pull-left btn btn-success mt-5 mb-5"
-  @click="addNewItem" >Add Item</button><br><br>
-	<form class="form-horizontal" role="form" method="POST" action="{{ route('transaction-mgmt.store') }}">
-	{{ csrf_field() }}
-	<div class="card" v-for="(employee, index) in employees">
-	<div class="col-md-10 col-lg-10">
-      <input type="button" class="pull-right btn btn-xs col-xs-1 btn-danger" value="X" @click="deleteForm">
-	      <div class="col-md-3 col-lg-3">
-			<label>Item</label>
-		    <input type="text" name="item_code[]" class="form-control" placeholder="Name" v-model="employee.name">
-		  </div>
-
-	      <div class="col-md-3 col-lg-3">
-			<label>Quantity</label>
-	        <input type="text" name="qty[]" class="form-control" placeholder="Job" v-model="employee.job">
-	      </div>
-
-	      <div class="col-md-3 col-lg-3">
-			<label>Unit Price</label>
-	        <input type="text" name="unit_price[]" class="form-control" placeholder="About" v-model="employee.about">
-	      </div>
-
-	      <div class="col-md-3 col-lg-3">
-			<label>TOtal Price</label>
-	        <input type="text" name="total_price[]" class="form-control" placeholder="About" v-model="employee.about">
-	      </div>
-	</div>
-    </div>
-    <button type="submit" class="btn btn-primary">
-        Create
-    </button>
-    </form>                            
-</div> <!-- end for v-if -->
+	</section>	
+</div>
+</body>
 @endsection
